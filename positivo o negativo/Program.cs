cc7bc18c0311e0f1979496972d54f563f0bb085c
yoga 11e <yoga 11e@DESKTOP-OOOUNS6>
2024-02-19 15:10:40 -0400

    Agregar archivos de proyecto.

---
+++ positivo o negativo/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("positio o negativo");
+Console.WriteLine();
+
+Console.Write("ingrese un numero:   ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+if (numero > 0)
+{
+    Console.WriteLine("el numero es positivo");
+}
+else
+{
+    Console.WriteLine("el numero es negativo");
+}
